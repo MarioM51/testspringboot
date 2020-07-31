4e0e05e90b11162d8b45fef626de9c69065871ee
Mario Marquez <mario51marquez@gmail.com>
2020-07-31 03:12:05 -0500

    Initial commit

---
+++ src/test/java/mario/testspringboot/testspringboot/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package mario.testspringboot.testspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
